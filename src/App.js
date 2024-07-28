f438aef65aebd6b345e505085c7f20ef9138c15a
Sushil Gupta <sushil.codepro@gmail.com>
2024-07-29 00:03:53 +0530

    all functionality done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
-
+import { items } from "./index";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [currInd, setCurrInd] = useState(0);
+  const [intervalID, setIntervalID]=useState(null);
+  const preHandle = () => {
+    if (currInd <= 0) {
+      return;
+    }
+    setCurrInd(currInd - 1);
+  }
+
+  const nextHandle = () => {
+
+    if (currInd >= items.length - 1) {
+      return;
+    }
+    setCurrInd(currInd + 1);
+  }
+  const resetHandle = () => {
+    if(intervalID){
+      clearInterval(intervalID);
+    }
+    
+  }
+  useEffect(()=>{
+      const interval=setInterval(()=>{
+        setCurrInd((pre)=>(pre+1 )% items.length);
+      },1000);
+      setIntervalID(interval);
+      return ()=>clearInterval(interval);
+  },[]);
   return (
-    <div className="App">
-      <h1>Hi</h1>
+    <div className="container mx-auto flex justify-center items-center px-4">
+      <button
+        onClick={preHandle}
+        type="button"
+        className="text-white bg-cyan-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >Pre
+      </button>
+      <div className="flex flex-col items-center mx-4">
+        <div className=''>
+          <img src={items[currInd].picUrl} alt={`${items[currInd].name}`} />
+        </div>
+        <div className="text-center">
+          {items[currInd].name}
+        </div>
+        <button
+        onClick={resetHandle}
+        type="button"
+        className="text-white bg-cyan-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >Stop
+      </button>
+      </div>
+      <button
+        onClick={nextHandle}
+        type="button"
+        className="text-white bg-cyan-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >Next
+      </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './styles/style.css';
 import App from './App';
 
+export const items = [
+  {
+    name: "John Doe",
+    picUrl: "https://picsum.photos/200/300"
+  },
+  {
+    name: "Jane Smith",
+    picUrl: "https://picsum.photos/id/237/200/300"
+  },
+  {
+    name: "Alice Johnson",
+    picUrl: "https://picsum.photos/seed/picsum/200/300"
+  },
+  {
+    name: "Bob Brown",
+    picUrl: "https://picsum.photos/200/300?grayscale"
+  },
+  {
+    name: "Charlie Davis",
+    picUrl: "https://picsum.photos/200/300/?blur"
+  }
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
